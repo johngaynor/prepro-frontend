db16235a7c5184fe90bd92a56b7aa6976627927f
johngaynor <john.gaynor@irvmat.com>
2024-07-07 16:48:34 -0400

    added auth protection on the frontend

--- src/App/Main/index.jsx
+++ src/App/Main/index.jsx
@@ -6,21 +6,45 @@ import Logs from "./components/DailyLogs/logs";
 import ExerciseLog from "./fitness/exerciseLog";
 import DailyLogForm from "./components/DailyLogForm/DailyLogForm";
 import AdminConsole from "./admin";
+import AppContext from "./components/Context/appContext";
 import { LogProvider } from "./components/Context/logContext";
+import Spinner from "./components/Spinner";
+import { Segment } from "semantic-ui-react";
 
 const Main = (props) => {
   const location = useLocation();
+  const { apps, user } = React.useContext(AppContext);
+
+  const withAuth = (Component, appId) => {
+    const match = apps.find((a) => a.id === appId);
+    if (match) {
+      return <Component {...props} />;
+    } else {
+      return (
+        <Segment>
+          <h4>
+            You do not have access to this app. Please contact your coach or
+            account manager to grant access.
+          </h4>
+        </Segment>
+      );
+    }
+  };
 
   return (
     <div style={{ margin: "1rem" }}>
+      {!apps && <Spinner />}
       <BreadCrumb path={location.pathname} />
       <LogProvider>
         <Routes>
           <Route path="/" element={<Homepage startsWith={"/"} />} />
-          <Route path="/admin" element={<AdminConsole />} />
-          <Route path="/logs" element={<Logs />} />
-          <Route path="/fitness/log" element={<ExerciseLog />} />
-          <Route path="/logs/new/:logType" element={<DailyLogForm />} />
+          <Route path="/admin" element={withAuth(AdminConsole, 1)} />
+          <Route path="/logs" element={withAuth(Logs, 2)} />
+          <Route path="/fitness/log" element={withAuth(ExerciseLog, 3)} />
+          <Route
+            path="/logs/new/:logType"
+            element={withAuth(DailyLogForm, 3)}
+          />
           <Route
             path="*"
             element={<Homepage startsWith={location.pathname} />}
