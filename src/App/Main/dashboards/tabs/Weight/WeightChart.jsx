61523773ac9a715574ff6a46c821f5dbe554bc71
johngaynor <john.gaynor@irvmat.com>
2024-11-05 19:20:14 -0500

    Added dropdown for chart control

--- src/App/Main/dashboards/tabs/Weight/WeightChart.jsx
+++ src/App/Main/dashboards/tabs/Weight/WeightChart.jsx
@@ -10,70 +10,84 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const WeightChart = ({
+  data = [],
+  type = "linear",
+  dot = true,
+  lineColor = "#000",
+  style = {},
+}) => {
+  const minValue = Math.min(
+    ...data.filter((d) => d.value).map((entry) => entry.value)
+  );
+  const maxValue = Math.max(
+    ...data.filter((d) => d.value).map((entry) => entry.value)
+  );
 
-const WeightChart = () => {
   return (
-    <ResponsiveContainer width={"100%"} height={300}>
+    <ResponsiveContainer width={"100%"} height={300} style={style}>
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
-        <YAxis />
+        <XAxis dataKey="date" padding={{ left: 30, right: 30 }} />
+        <YAxis domain={[Math.floor(minValue - 1), Math.ceil(maxValue + 1)]} />
         <Tooltip />
         <Legend />
         <Line
-          type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
+          type={type}
+          dataKey="value"
+          stroke={lineColor}
+          dot={dot}
+          name="Weight (lbs)"
         />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
       </LineChart>
     </ResponsiveContainer>
   );
 };
 
 export default WeightChart;
+
+// const data = [
+//   {
+//     name: "Page A",
+//     uv: 4000,
+//     pv: 2400,
+//     amt: 2400,
+//   },
+//   {
+//     name: "Page B",
+//     uv: 3000,
+//     pv: 1398,
+//     amt: 2210,
+//   },
+//   {
+//     name: "Page C",
+//     uv: 2000,
+//     pv: 9800,
+//     amt: 2290,
+//   },
+//   {
+//     name: "Page D",
+//     uv: 2780,
+//     pv: 3908,
+//     amt: 2000,
+//   },
+//   {
+//     name: "Page E",
+//     uv: 1890,
+//     pv: 4800,
+//     amt: 2181,
+//   },
+//   {
+//     name: "Page F",
+//     uv: 2390,
+//     pv: 3800,
+//     amt: 2500,
+//   },
+//   {
+//     name: "Page G",
+//     uv: 3490,
+//     pv: 4300,
+//     amt: 2100,
+//   },
+// ];
--- src/App/Main/dashboards/tabs/Weight/index.jsx
+++ src/App/Main/dashboards/tabs/Weight/index.jsx
@@ -4,6 +4,15 @@ import Spinner from "../../../components/Spinner";
 import { connect } from "react-redux";
 import WeightChart from "./WeightChart";
 import { getWeightLogs } from "../../../nutrition/actions";
+import { DateTime } from "luxon";
+import { DropdownField } from "../../../components/FormFields";
+
+const smallOptions = [
+  { text: "Last 7 Days", value: 7 },
+  { text: "Last 14 Days", value: 14 },
+  { text: "Last 30 Days", value: 30 },
+  { text: "Last 60 Days", value: 60 },
+];
 
 const Weight = ({
   // from redux
@@ -18,38 +27,91 @@ const Weight = ({
     if (!weightLogs && !logsLoading) getWeightLogs();
   }, [weightLogs, logsLoading]);
 
-  console.log(weightLogs);
+  const primaryData = weightLogs
+    ? Array.from({ length: primary })
+        .map((_, index) => {
+          const date = DateTime.now().minus({ days: index }).startOf("day");
+          const log = weightLogs.find((l) => l.date === date.toISODate());
+          return {
+            date: date.toFormat("MMM dd"),
+            value: log?.weight || null,
+          };
+        })
+        .reverse()
+    : [];
+
+  const secondaryData = weightLogs
+    ? Array.from({ length: secondary })
+        .map((_, index) => {
+          const date = DateTime.now().minus({ days: index }).startOf("day");
+          const log = weightLogs.find((l) => l.date === date.toISODate());
+          return {
+            date: date.toFormat("MMM dd"),
+            value: log?.weight || null,
+          };
+        })
+        .reverse()
+    : [];
+
+  const oldestDate = weightLogs
+    ? weightLogs
+        .map((log) => DateTime.fromISO(log.date))
+        .reduce((min, current) => (current < min ? current : min))
+    : null;
+
+  const allTime = weightLogs
+    ? Array.from({ length: DateTime.now().diff(oldestDate, "days").days + 1 })
+        .map((_, index) => {
+          const date = DateTime.now().minus({ days: index }).startOf("day");
+          const log = weightLogs.find((l) => l.date === date.toISODate());
+          return {
+            date: date.toFormat("MMM dd, yyyy"),
+            value: log?.weight || null,
+          };
+        })
+        .reverse()
+    : [];
 
   return (
     <Tab.Pane>
       {logsLoading && <Spinner />}
-      <Header as="h2">Weight Logs</Header>
-      {/* <DropdownField
-        label="Selected User"
-        placeholder="Select a user to manage..."
-        options={
-          apiUsers
-            ? apiUsers.map((u) => ({
-                text: u.name + " - " + u.email,
-                value: u.id,
-              }))
-            : []
-        }
-        value={activeUser}
-        onChange={(e, { value }) => setActiveUser(value)}
-      /> */}
       <Grid columns={2}>
         <Grid.Column>
           <Segment>
-            <WeightChart />
+            <DropdownField
+              options={smallOptions}
+              value={primary}
+              onChange={(e, { value }) => setPrimary(value)}
+            />
+            <WeightChart
+              data={primaryData}
+              lineColor="#06AED5"
+              style={{ marginTop: 30 }}
+            />
           </Segment>
         </Grid.Column>
         <Grid.Column>
           <Segment>
-            <WeightChart />
+            <DropdownField
+              options={smallOptions}
+              value={secondary}
+              onChange={(e, { value }) => setSecondary(value)}
+            />
+            <WeightChart
+              data={secondaryData}
+              lineColor="#086788"
+              style={{ marginTop: 30 }}
+            />
           </Segment>
         </Grid.Column>
       </Grid>
+      <Segment>
+        <Header as="h2">
+          All Time ({oldestDate?.toFormat("MMM dd, yyyy")} -{" "}
+          {DateTime.now().toFormat("MMM dd, yyyy")})
+        </Header>
+        <WeightChart data={allTime} dot={false} lineColor="#078BAF" />
+      </Segment>
     </Tab.Pane>
   );
 };
