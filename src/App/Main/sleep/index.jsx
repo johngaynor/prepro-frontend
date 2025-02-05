54c30d5bdd730f4dc4c0894fbc9fa2272120156b
johngaynor <john.gaynor@irvmat.com>
2025-02-05 13:52:22 -0500

    Created template for sleep app

---
+++ src/App/Main/sleep/index.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import Tab from "../components/Tab";
+import Spinner from "../components/Spinner";
+import { connect } from "react-redux";
+import { getSleepLogs } from "./actions";
+import { useNavigate, useParams } from "react-router-dom";
+
+const SleepApp = ({ sleepLogs, sleepLogsLoading, getSleepLogs }) => {
+  useEffect(() => {
+    if (!sleepLogs && !sleepLogsLoading) getSleepLogs();
+  }, [sleepLogs, sleepLogsLoading, getSleepLogs]);
+
+  const { maintab } = useParams();
+  const navigate = useNavigate();
+
+  const mainPanes = [
+    {
+      menuItem: "Statistics",
+      render: () => {
+        return "Statistics";
+      },
+    },
+    {
+      menuItem: "Integrations",
+      render: () => {
+        return "Integrations";
+      },
+    },
+  ];
+
+  const activeTab = mainPanes.findIndex(
+    (pane) => pane.menuItem.toLowerCase() === maintab
+  );
+  return (
+    <>
+      {sleepLogsLoading && <Spinner />}
+      <Tab
+        panes={mainPanes}
+        activeIndex={activeTab < 0 ? 0 : activeTab}
+        onTabChange={(e, { activeIndex }) =>
+          navigate(`/sleep/${mainPanes[activeIndex].menuItem.toLowerCase()}`)
+        }
+      />
+    </>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    sleepLogs: state.sleep.logs,
+    sleepLogsLoading: state.sleep.logsLoading,
+  };
+}
+
+export default connect(mapStateToProps, { getSleepLogs })(SleepApp);
