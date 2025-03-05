ceaa510848c46dd576a929babcaa63a521a29622
johngaynor <john.gaynor@irvmat.com>
2025-03-05 10:22:15 -0500

    commented out constant refresh

--- src/main.jsx
+++ src/main.jsx
@@ -16,18 +16,18 @@ const updateSW = registerSW({
   },
 });
 
-// Clear any old service workers and caches on app load
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      registration.unregister(); // Unregister old service workers
-    });
-  });
+// // Clear any old service workers and caches on app load, commented out because it was creating a constant cycle of refreshing
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.getRegistrations().then((registrations) => {
+//     registrations.forEach((registration) => {
+//       registration.unregister(); // Unregister old service workers
+//     });
+//   });
 
-  caches.keys().then((names) => {
-    names.forEach((name) => caches.delete(name)); // Delete all cached assets
-  });
-}
+//   caches.keys().then((names) => {
+//     names.forEach((name) => caches.delete(name)); // Delete all cached assets
+//   });
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
