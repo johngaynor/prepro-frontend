2bda94ea43890cea6bbd120d85f467a568b87295
johngaynor <john.gaynor@irvmat.com>
2025-04-27 15:50:03 -0400

    attempted to fix caching issue

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,15 +2,30 @@ import Main from "./Main";
 import Navbar from "./Main/components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 import { ErrorBoundary } from "react-error-boundary";
 import DefaultError from "./Main/components/Errors/DefaultError";
+import { registerSW } from "virtual:pwa-register";
 
 const store = configureStore();
 
 function App() {
+  useEffect(() => {
+    console.log("Checking service worker...");
+    registerSW({
+      onNeedRefresh() {
+        if (confirm("New version available. Reload?")) {
+          window.location.reload();
+        }
+      },
+      onOfflineReady() {
+        console.log("App ready to work offline");
+      },
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
--- src/main.jsx
+++ src/main.jsx
@@ -4,28 +4,6 @@ import App from "./App/App.jsx";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { registerSW } from "virtual:pwa-register";
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content is available. Reload?")) {
-      updateSW(true);
-    }
-  },
-});
-
-// // Clear any old service workers and caches on app load, commented out because it was creating a constant cycle of refreshing
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker.getRegistrations().then((registrations) => {
-//     registrations.forEach((registration) => {
-//       registration.unregister(); // Unregister old service workers
-//     });
-//   });
-
-//   caches.keys().then((names) => {
-//     names.forEach((name) => caches.delete(name)); // Delete all cached assets
-//   });
-// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
